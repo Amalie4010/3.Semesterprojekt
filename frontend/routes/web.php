791b42088bb979f6dd99772fe0f261ccf9841b7a
Mohammed Hamarash <mohammed_hamarashi@hotmail.com>
2025-10-23 09:49:46 +0200

    file structure is reworked so order system is in frontend and backend is online statistic and communication

--- frontend/routes/web.php
+++ frontend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use Illuminate\Routing\Controllers\Middleware;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* When you call the api from frontend REMEMBER!!!!! to have /api at front of /order.. like this http://localhost:8000/api/order
+Because /api is prefixed in api routes file */
+
+Route::get('/api/orders', [OrderController::class, 'getOrders']);
+Route::post('/api/order', [OrderController::class, 'createOrder']);
