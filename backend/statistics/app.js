0dc406686232fbaa6a2fb0adaf4e8065316f073b
Freya Andersen <freyasecherandersen@gmail.com>
2025-10-27 14:54:33 +0100

    removed .env port

--- backend/statistics/app.js
+++ backend/statistics/app.js
@@ -5,7 +5,7 @@ const app = express ();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 //Use routes
 app.use("/api/statistics", routes);
