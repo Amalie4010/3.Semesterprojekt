cf33bf31f72322aa3f5d019ce8693b3dae09f65c
Joac2363 <joachimstorbank@gmail.com>
2025-10-28 20:38:12 +0100

    Added more PackML commands

---
+++ backend/communication/Communication/CrtllCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace communication.Communication
+{
+    public static class CrtllCommand
+    {
+        public static readonly int Reset = 1;
+        public static readonly int Start = 2;
+        public static readonly int Stop = 3;
+        public static readonly int Abort = 4;
+        public static readonly int Clear = 5;
+    }
+}
---
+++ backend/communication/Communication/MachineState.cs
@@ -0,0 +1,12 @@
+﻿namespace communication.Communication
+{
+    public static class MachineState
+    {
+        public static readonly int Idle = 4;
+        public static readonly int Held = 11;
+        public static readonly int Completed = 17;
+        public static readonly int Execute = 6;
+        public static readonly int Stopped = 2;
+        public static readonly int Aborted = 9;
+    }
+}
