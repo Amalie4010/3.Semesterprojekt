c0f8bbd695816c0b0a3a076d3857f42d45e00b2a
Freya Andersen <freyasecherandersen@gmail.com>
2025-11-05 10:30:11 +0100

    Statistics DB Sqlite

--- backend/statistics/app.js
+++ backend/statistics/app.js
@@ -1,11 +1,16 @@
 import routes from "./routes/routes.js";
 import express from "express";
+
+//Initialize Database
+import './database/create_database.js'; //Creates the database
+
+//Start server
 const app = express ();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const PORT = 8080;
+const PORT = 8080; //Do not change - port has been agreed upon in the group
 
 //Use routes
 app.use("/api", routes);
@@ -15,6 +20,5 @@ app.use("/api", routes);
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
 });
-
 
 
---
+++ backend/statistics/database/create_database.js
@@ -0,0 +1,17 @@
+import sqlite3 from 'sqlite3';
+import path from 'path';
+
+//Create the database
+const db = new sqlite3.Database('./database/event.db');
+
+//Creates a table for the current event
+let comand = `CREATE TABLE IF NOT EXISTS current_event (
+	id INTEGER PRIMARY KEY,
+	beer_type INTEGER NOT NULL,
+  amount INTEGER NOT NULL
+);`;
+
+// Execute the command
+db.exec(comand);
+
+//db.exec("insert into current_event (beer_type,amount) values(2,8)");
