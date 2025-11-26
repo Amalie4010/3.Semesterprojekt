9e5a9bf1ad6983a9b1c425222bde315a05b56e4c
RaminJ9 <raminjaved@gmail.com>
2025-11-26 16:28:30 +0100

    remade a ordermigration, and made the beertypes put into the database with the seeder.

--- frontend/database/seeders/BeerTypeSeeder.php
+++ frontend/database/seeders/BeerTypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BeerTypeSeeder extends Seeder
 {
@@ -12,6 +13,35 @@ class BeerTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // these are the types of beer we sell, they will be shown in the attendee page
+        DB::table('beertype')->insert([
+            [
+                'type_id'=>0,
+                'name'=>'Pilsner'
+            ],
+            [
+                'type_id'=>1,
+                'name'=>'Wheat'  
+            ],
+            [
+                'type_id'=>2,
+                'name'=>'IPA'
+            ],
+            [
+                'type_id'=>3,
+                'name'=>'Staut'
+            ],
+            [
+                'type_id'=>4,
+                'name'=>'Ale'
+            ],
+            [
+                'type_id'=>5,
+                'name'=>'Alkoholfri'
+            ]
+        ]);
+        /* now what i gotta do is make it so the dropdownbox shows foreach item in the table.
+            and when you make an order it will send the "type_id", instead of the hardcoded value inside of the 
+        */
     }
 }
